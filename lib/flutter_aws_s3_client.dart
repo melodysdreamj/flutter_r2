e4428461716e1b3d4f3c5cbf9d22f46044cc5aec
june lee <melodysdreamj@gmail.com>
2022-09-23 21:26:55 +0900

    Initial commit

---
+++ lib/flutter_aws_s3_client.dart
@@ -0,0 +1,6 @@
+library flutter_aws_s3_client;
+
+export 'src/client/client.dart';
+export 'src/client/exceptions.dart';
+export 'src/model/list_bucket_result.dart';
+export 'src/model/list_bucket_result_contents.dart';
